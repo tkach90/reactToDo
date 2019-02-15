187175fec6d084af4fd843aa3960e09989df362d
vlad.tkach <vlad.tkach@asmbr.com>
2019-02-15 07:51:07 +0200

    fix bug with path in App.js

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../../reactToDo/todo-list/src/App';
+import App from '../src/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
